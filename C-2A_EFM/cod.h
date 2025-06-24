8a888a192dcbd0885c20dba0f1966da4db185194
Pablo-Richardson <pabloorichardson@gmail.com>
2025-06-24 11:54:40 +0100

    working on class

---
+++ C-2A_EFM/cod.h
@@ -0,0 +1,10 @@
+#pragma once
+//Build upon this
+//Need objects to make up aircraft - Gear, wings (flaps integrated), elevators, ailerons, rudders, engines, body
+class Cod
+{
+public:
+
+private:
+	
+};
