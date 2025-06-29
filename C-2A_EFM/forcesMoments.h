5a7198425c311ff8e5a31d3a829f35ca158d6aaf
Pablo-Richardson <pabloorichardson@gmail.com>
2025-06-29 18:25:16 +0100

    added operators

--- C-2A_EFM/forcesMoments.h
+++ C-2A_EFM/forcesMoments.h
@@ -2,9 +2,21 @@
 struct ForcesMoments {
     double force[3];   // body axis
     double moment[3];  // body axis
+
+    ForcesMoments operator+(const ForcesMoments& other) const {
+        ForcesMoments result;
+        for (int i = 0; i < 3; ++i) {
+            result.force[i] = this->force[i] + other.force[i];
+            result.moment[i] = this->moment[i] + other.moment[i];
+        }
+        return result;
+    }
+
+    ForcesMoments& operator+=(const ForcesMoments& other) {
+        for (int i = 0; i < 3; ++i) {
+            force[i] += other.force[i];
+            moment[i] += other.moment[i];
+        }
+        return *this;
+    }
 };
-
-double addForces()
-{
-
-}
