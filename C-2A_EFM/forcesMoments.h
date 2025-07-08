1a87403c8280c4cb8543777baaab1409a33cce2e
Pablo-Richardson <pabloorichardson@gmail.com>
2025-07-08 15:09:57 +0100

    forces moments uses Vec3

--- C-2A_EFM/forcesMoments.h
+++ C-2A_EFM/forcesMoments.h
@@ -1,35 +1,42 @@
 #pragma once
+#include "Vec3.h"
 // This struct is my custom struct allowing for forces and moments to be added up
 struct ForcesMoments {
-    double force[3];   // body axis
-    double moment[3];  // body axis
+    Vec3 force;   // body axis
+    Vec3 moment;  // body axis
 
 	// Addition operator for ForcesMoments
     ForcesMoments operator+(const ForcesMoments& other) const {
         ForcesMoments result;
-        for (int i = 0; i < 3; ++i) {
+        /*for (int i = 0; i < 3; ++i) {
             result.force[i] = this->force[i] + other.force[i];
             result.moment[i] = this->moment[i] + other.moment[i];
-        }
+        }*/
+		result.force = this->force + other.force;
+		result.moment = this->moment + other.moment;
         return result;
     }
 
 	// Addition assignment operator for ForcesMoments
     ForcesMoments& operator+=(const ForcesMoments& other) {
-        for (int i = 0; i < 3; ++i) {
+        /*for (int i = 0; i < 3; ++i) {
             force[i] += other.force[i];
             moment[i] += other.moment[i];
-        }
+        }*/
+		force += other.force;
+		moment += other.moment;
         return *this;
     }
 	// Default constructor initializes forces and moments to zero
     ForcesMoments() : force{ 0.0, 0.0, 0.0 }, moment{ 0.0, 0.0, 0.0 } {}
 	// Constructor to initialize forces and moments with given values
-    ForcesMoments(const double f[3], const double m[3]) {
-        for (int i = 0; i < 3; ++i) {
+    ForcesMoments(const Vec3 f, const Vec3 m) {
+        /*for (int i = 0; i < 3; ++i) {
             force[i] = f[i];
             moment[i] = m[i];
-        }
+        }*/
+		force = f;
+		moment = m;
     }
 };
 // Function to sum all forces and moments
