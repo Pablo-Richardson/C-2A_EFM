0da296e6d8f08c5a59fee3d1e170c93fe67c7df4
Pablo-Richardson <pabloorichardson@gmail.com>
2025-07-09 14:48:31 +0100

    massProperties uses Vec3

--- C-2A_EFM/massProperties.h
+++ C-2A_EFM/massProperties.h
@@ -1,12 +1,53 @@
 #pragma once
-// I wont update these, and just assume a centered COM, but you can use a file like this to update the COM for moment calculations
-// You can also use it to update the mass of the aircraft based on fuel - realism (make sure to account for fuel mass when the aircraft is initialised!)
+#include "Vec3.h"
+/*
+I wont update these, and just assume a centered COM, but you can use a file like this to update the COM for moment calculations
+You can also use it to update the mass of the aircraft based on fuel - realism 
+(make sure to account for fuel mass when the aircraft is initialised!)
+*/
+
+class massProperties {
+public:
+	// Default constructor with default COM and mass
+	massProperties() : com{ 0.0, 0.0, 0.0 }, mass(25000.0) {}
+	// Constructor to initialize with specific COM and mass
+	massProperties(const Vec3& centerOfMass, double massValue) : com(centerOfMass), mass(massValue) {}
+	// Update the mass and COM
+	void update(const Vec3& centerOfMass, double massValue) { com = centerOfMass; mass = massValue; }
+	// Set COM
+	void setCOM(const Vec3& centerOfMass) { com = centerOfMass; }
+	// Set mass
+	void setMass(double massValue) { mass = massValue; }
+	// Get COM
+	const Vec3& getCOM() const { return com; }
+	// Get mass
+	const double& getMass() const { return mass; }
+private:
+	Vec3 com; // Center of Mass in local coordinates
+	double mass; // Mass in kg
+};
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 struct massState {
-	double mass, COM[3];
+	double mass; // Mass in kg
+	Vec3 com; // Center of Mass in local coordinates
 };
 
 class MassProperties {
 public:
+	MassProperties() = default;
+	MassProperties(const massState& mass) : data(mass) {}
 	void update(const massState& mass) { data = mass; }
 	const massState& get() const { return data; }
 	void updateMass(const double mass) { data.mass = mass; }
@@ -14,3 +55,4 @@ public:
 private:
 	massState data = { 25000, {0.0, 0.0, 0.0} }; // Default mass and COM
 };
+*/
