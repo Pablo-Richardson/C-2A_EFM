b3f50400cdbe78f22e797655024cecb196de1121
Pablo-Richardson <pabloorichardson@gmail.com>
2025-07-10 13:08:11 +0100

    finished atmosphere

--- C-2A_EFM/atmosphere.h
+++ C-2A_EFM/atmosphere.h
@@ -2,7 +2,7 @@
 #include "constants.h"
 #include "Vec3.h"
 // Keeps track of atmospheric conditions and provides them to the flight model - Recieves data from DCS
-// Replace array with vector!
+// Done! :D
 
 class Atmosphere
 {
@@ -13,39 +13,27 @@ public:
 	Atmosphere(const Vec3& wind_s, double density_s, double temperature_s, double pressure_s, double altitude_s)
 		: wind(wind_s), density(density_s), temperature(temperature_s), pressure(pressure_s), altitude(altitude_s) {}
 	// Update atmospheric conditions - DCS
-
+	void update(const Vec3& wind_s, double density_s, double temperature_s, double pressure_s, double altitude_s) {
+		wind = wind_s;
+		density = density_s;
+		temperature = temperature_s;
+		pressure = pressure_s;
+		altitude = altitude_s;
+	}
+	// Getters for individual atmospheric properties
+	Vec3 getWind() const { return wind; }
+	double getDensity() const { return density; }
+	double getTemperature() const { return temperature; }
+	double getPressure() const { return pressure; }
+	double getAltitude() const { return altitude; }
+	// Get wind componenets
+	double getWindX() const { return wind.x; }
+	double getWindY() const { return wind.y; }
+	double getWindZ() const { return wind.z; }
 private:
 	Vec3 wind; // Wind vector in m/s
 	double density; // Density in kg/m^3
 	double temperature; // Temperature in Kelvin
 	double pressure; // Pressure in Pascals
 	double altitude; // Altitude in meters
-
 };
-
-
-
-/*
-struct AtmosphereData {
-	double dens, alt, temp, press, wind[3];
-};
-
-class Atmosphere
-{
-public:
-	void update(const AtmosphereData& data) { this->data = data; }
-	const AtmosphereData& get() const { return data; }
-	// Getters for individual atmospheric properties
-	double getDensity() const { return data.dens; }
-	double getAltitude() const { return data.alt; }
-	double getTemperature() const { return data.temp; }
-	double getPressure() const { return data.press; }
-	double getWindX() const { return data.wind[0]; }
-	double getWindY() const { return data.wind[1]; }
-	double getWindZ() const { return data.wind[2]; }
-	// Default constructor
-	Atmosphere() : data{} {}
-private:
-	AtmosphereData data;
-};
-*/
