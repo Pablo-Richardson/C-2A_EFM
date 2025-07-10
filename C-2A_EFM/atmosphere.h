5df664727980f262889b9d86cb7db32db640a27c
Pablo-Richardson <pabloorichardson@gmail.com>
2025-07-10 09:23:47 +0100

    revamping atmosphere object

--- C-2A_EFM/atmosphere.h
+++ C-2A_EFM/atmosphere.h
@@ -1,7 +1,31 @@
 #pragma once
 #include "constants.h"
+#include "Vec3.h"
 // Keeps track of atmospheric conditions and provides them to the flight model - Recieves data from DCS
 // Replace array with vector!
+
+class Atmosphere
+{
+public:
+	// Empty Constructor
+	Atmosphere() : wind{0, 0, 0}, density(1.225), temperature(288.15), pressure(101325), altitude(0) {}
+	// Full Constructor
+	Atmosphere(const Vec3& wind_s, double density_s, double temperature_s, double pressure_s, double altitude_s)
+		: wind(wind_s), density(density_s), temperature(temperature_s), pressure(pressure_s), altitude(altitude_s) {}
+	// Update atmospheric conditions - DCS
+
+private:
+	Vec3 wind; // Wind vector in m/s
+	double density; // Density in kg/m^3
+	double temperature; // Temperature in Kelvin
+	double pressure; // Pressure in Pascals
+	double altitude; // Altitude in meters
+
+};
+
+
+
+/*
 struct AtmosphereData {
 	double dens, alt, temp, press, wind[3];
 };
@@ -24,3 +48,4 @@ public:
 private:
 	AtmosphereData data;
 };
+*/
